faa068056bd7371da90e048f45623bd811ced068
Nbneeruga <bharath.neeruganti@metc.state.mn.us>
2025-04-10 15:22:10 -0500

    terraform files added

---
+++ variables.tf
@@ -0,0 +1,26 @@
+```terraform
+## 
+# Variables
+##
+
+variable "location" {
+    type = string
+    default = "uksouth"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "techielassgha"
+}
+
+variable "tag_environment" {
+  type    = string
+  default = "test"
+}
+
+variable "tag_owner" {
+  type    = string
+  default = "sarah"
+}
+
+```
